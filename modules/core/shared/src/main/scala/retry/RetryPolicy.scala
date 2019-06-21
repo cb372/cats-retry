2437ba2bd7cf2662c9e668b3f1e127b2ce0653a1
Luka Jacobowitz <luka.jacobowitz@gmail.com>
2019-06-20 21:00:04 -0400

    Add meet

--- modules/core/shared/src/main/scala/retry/RetryPolicy.scala
+++ modules/core/shared/src/main/scala/retry/RetryPolicy.scala
@@ -5,8 +5,26 @@ import cats.kernel.BoundedSemilattice
 import retry.PolicyDecision._
 
 import scala.concurrent.duration.Duration
+import cats.Apply
 
-case class RetryPolicy[M[_]](decideNextRetry: RetryStatus => M[PolicyDecision])
+case class RetryPolicy[M[_]](
+    decideNextRetry: RetryStatus => M[PolicyDecision]) {
+  def join(rp: RetryPolicy[M])(implicit M: Apply[M]): RetryPolicy[M] =
+    RetryPolicy[M](status =>
+      M.map2(decideNextRetry(status), rp.decideNextRetry(status)) {
+        case (DelayAndRetry(a), DelayAndRetry(b)) => DelayAndRetry(a max b)
+        case _                                    => GiveUp
+    })
+
+  def meet(rp: RetryPolicy[M])(implicit M: Apply[M]): RetryPolicy[M] =
+    RetryPolicy[M](status =>
+      M.map2(decideNextRetry(status), rp.decideNextRetry(status)) {
+        case (DelayAndRetry(a), DelayAndRetry(b)) => DelayAndRetry(a min b)
+        case (s @ DelayAndRetry(_), GiveUp)       => s
+        case (GiveUp, s @ DelayAndRetry(_))       => s
+        case _                                    => GiveUp
+    })
+}
 
 object RetryPolicy {
 
@@ -29,16 +47,7 @@ object RetryPolicy {
       override def combine(
           x: RetryPolicy[M],
           y: RetryPolicy[M]
-      ): RetryPolicy[M] = RetryPolicy[M](
-        retryStatus =>
-          M.map2(
-            x.decideNextRetry(retryStatus),
-            y.decideNextRetry(retryStatus)
-          ) {
-            case (DelayAndRetry(a), DelayAndRetry(b)) => DelayAndRetry(a max b)
-            case _                                    => GiveUp
-        }
-      )
+      ): RetryPolicy[M] = x.join(y)
     }
 
 }
