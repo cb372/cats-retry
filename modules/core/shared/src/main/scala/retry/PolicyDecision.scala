b2d5473976bc6996360b3f3dca0a9dd8c33e8132
Cristian Cantarero <cristian.cantarerodavila@siriusxm.com>
2024-11-28 16:57:28 +0100

    fix: Remove not needed val on PolicyDecision

--- modules/core/shared/src/main/scala/retry/PolicyDecision.scala
+++ modules/core/shared/src/main/scala/retry/PolicyDecision.scala
@@ -1,8 +1,7 @@
 package retry
 
 import scala.concurrent.duration.FiniteDuration
-import scala.concurrent.duration.DurationInt
 
-enum PolicyDecision(val delay: FiniteDuration):
-  case GiveUp                                            extends PolicyDecision(0.seconds)
-  case DelayAndRetry(override val delay: FiniteDuration) extends PolicyDecision(delay)
+enum PolicyDecision:
+  case GiveUp
+  case DelayAndRetry(delay: FiniteDuration)
