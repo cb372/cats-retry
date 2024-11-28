e4b938911adb58f1a1188471a950fa6d2fb34157
Chris Birchall <chris.birchall@47deg.com>
2024-11-28 17:40:32 +0000

    Update the extension methods

--- modules/core/shared/src/main/scala/retry/syntax/retry.scala
+++ modules/core/shared/src/main/scala/retry/syntax/retry.scala
@@ -1,7 +1,7 @@
 package retry.syntax
 
 import cats.{Monad, MonadError}
-import retry.{RetryDetails, RetryPolicy, Sleep}
+import retry.{ResultHandler, RetryDetails, RetryPolicy, Sleep}
 
 trait RetrySyntax:
   // TODO how to translate these to methods to scala3?
@@ -16,28 +16,16 @@ trait RetrySyntax:
     new RetryingErrorOps[M, A, E](action)
 
 final class RetryingOps[M[_], A](action: => M[A]):
-  @deprecated("Use retryingOnFailures instead", "2.1.0")
-  def retryingM[E](
-      wasSuccessful: A => M[Boolean],
-      policy: RetryPolicy[M],
-      onFailure: (A, RetryDetails) => M[Unit]
-  )(using
-      M: Monad[M],
-      S: Sleep[M]
-  ): M[A] = retryingOnFailures(wasSuccessful, policy, onFailure)
-
   def retryingOnFailures[E](
-      wasSuccessful: A => M[Boolean],
       policy: RetryPolicy[M],
-      onFailure: (A, RetryDetails) => M[Unit]
+      resultHandler: ResultHandler[M, A, A]
   )(using
       M: Monad[M],
       S: Sleep[M]
   ): M[A] =
     retry.retryingOnFailures(
       policy = policy,
-      wasSuccessful = wasSuccessful,
-      onFailure = onFailure
+      resultHandler = resultHandler
     )(action)
 
 final class RetryingErrorOps[M[_], A, E](action: => M[A])(using
@@ -53,41 +41,31 @@ final class RetryingErrorOps[M[_], A, E](action: => M[A])(using
     )(action)
 
   def retryingOnSomeErrors(
-      isWorthRetrying: E => M[Boolean],
       policy: RetryPolicy[M],
-      onError: (E, RetryDetails) => M[Unit]
+      errorHandler: ResultHandler[M, E, A]
   )(using S: Sleep[M]): M[A] =
     retry.retryingOnSomeErrors(
       policy = policy,
-      isWorthRetrying = isWorthRetrying,
-      onError = onError
+      errorHandler = errorHandler
     )(action)
 
   def retryingOnFailuresAndAllErrors(
-      wasSuccessful: A => M[Boolean],
       policy: RetryPolicy[M],
-      onFailure: (A, RetryDetails) => M[Unit],
+      resultHandler: ResultHandler[M, A, A],
       onError: (E, RetryDetails) => M[Unit]
   )(using S: Sleep[M]): M[A] =
     retry.retryingOnFailuresAndAllErrors(
       policy = policy,
-      wasSuccessful = wasSuccessful,
-      onFailure = onFailure,
+      resultHandler = resultHandler,
       onError = onError
     )(action)
 
   def retryingOnFailuresAndSomeErrors(
-      wasSuccessful: A => M[Boolean],
-      isWorthRetrying: E => M[Boolean],
       policy: RetryPolicy[M],
-      onFailure: (A, RetryDetails) => M[Unit],
-      onError: (E, RetryDetails) => M[Unit]
+      resultOrErrorHandler: ResultHandler[M, Either[E, A], A]
   )(using S: Sleep[M]): M[A] =
     retry.retryingOnFailuresAndSomeErrors(
       policy = policy,
-      wasSuccessful = wasSuccessful,
-      isWorthRetrying = isWorthRetrying,
-      onFailure = onFailure,
-      onError = onError
+      resultOrErrorHandler = resultOrErrorHandler
     )(action)
 end RetryingErrorOps
